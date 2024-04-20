048c0d7f6bc667641e3e46c9118ef9bf7495d934
Barros313 <gbarros1337@gmail.com>
2024-04-19 22:36:37 -0300

    Attempt to submit maps form (not working)

--- scripts/location.js
+++ scripts/location.js
@@ -1,7 +1,11 @@
 const addLocationIcon = document.getElementById("add-location");
 const mapsContainer = document.getElementById("maps-container");
 const closeMapsIcon = document.getElementById("close-maps-container");
-
+const insertAddressButton = mapsContainer.querySelector('button');
+let projectScripts = document.getElementsByTagName('script');
+let apiScript = projectScripts[projectScripts.length - 4];
+let scriptURL = apiScript.src;
+console.log(apiScript);
 
 addLocationIcon.addEventListener("click", () => {
     mapsContainer.style.display = "block";
@@ -11,4 +15,20 @@ addLocationIcon.addEventListener("click", () => {
 closeMapsIcon.addEventListener("click", () => {
     mapsContainer.style.display = "none";
     addLocationIcon.style.display = "block";
+});
+
+insertAddressButton.addEventListener("click", () => {
+    const address = document.getElementById('location-input').value;
+    const aptSuite = document.getElementById('apt-suite-input').value;
+    const city = document.getElementById('locality-input').value;
+    const stateProvince = document.getElementById('administrative_area_level_1-input').value;
+    const zipPostalCode = document.getElementById('postal_code-input').value;
+    const country = document.getElementById('country-input').value;
+  
+    console.log('Address:', address);
+    console.log('Apt/Suite:', aptSuite);
+    console.log('City:', city);
+    console.log('State/Province:', stateProvince);
+    console.log('Zip/Postal Code:', zipPostalCode);
+    console.log('Country:', country);
 });
